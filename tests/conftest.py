0505bcf35011c6eb2defc39a02f648410176a157
Dag Evensberget <dag.evensberget@gmail.com>
2026-03-16 14:52:31 +0100

    Use marker filtering for pooch tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,24 +11,6 @@ from batcamp.octree import DEFAULT_AXIS_RHO_TOL
 from sample_data_helper import data_file
 
 
-def pytest_addoption(parser: pytest.Parser) -> None:
-    parser.addoption(
-        "--run-pooch",
-        action="store_true",
-        default=False,
-        help="run tests that fetch files via pooch/network",
-    )
-
-
-def pytest_collection_modifyitems(config: pytest.Config, items: list[pytest.Item]) -> None:
-    if config.getoption("--run-pooch"):
-        return
-    skip_pooch = pytest.mark.skip(reason="need --run-pooch option to run")
-    for item in items:
-        if "pooch" in item.keywords:
-            item.add_marker(skip_pooch)
-
-
 @lru_cache(maxsize=1)
 def _build_difflevels_rpa_context() -> dict[str, object]:
     """Private test helper: build and cache shared spherical test context."""
