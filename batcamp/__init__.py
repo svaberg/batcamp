6cdbde55df391f6a57e35da8c9b9d6a3837cbf06
Dag Evensberget <dag.evensberget@gmail.com>
2026-03-30 13:42:08 +0200

    Trim top-level package exports

--- batcamp/__init__.py
+++ batcamp/__init__.py
@@ -2,28 +2,12 @@
 
 from __future__ import annotations
 
-from .builder import DEFAULT_AXIS_RHO_TOL
-from .builder import DEFAULT_MIN_VALID_CELL_FRACTION
-from .builder import build_octree
 from .builder import build_octree_from_ds
-from .builder import format_histogram
-from .builder import point_refinement_levels
-from .builder import valid_cell_fraction
-from .constants import DEFAULT_TREE_COORD
 from .interpolator import OctreeInterpolator
 from .octree import Octree
-from .persistence import OCTREE_FILE_VERSION
 
 __all__ = [
-    "build_octree",
     "build_octree_from_ds",
-    "format_histogram",
-    "point_refinement_levels",
-    "valid_cell_fraction",
-    "DEFAULT_AXIS_RHO_TOL",
-    "DEFAULT_TREE_COORD",
-    "DEFAULT_MIN_VALID_CELL_FRACTION",
-    "OCTREE_FILE_VERSION",
     "Octree",
     "OctreeInterpolator",
 ]
